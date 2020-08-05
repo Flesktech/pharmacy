a1a45eee357aaadc434d28c6de852f4b45c66cf9
Ouches <53819265+Flesktech@users.noreply.github.com>
2020-08-05 06:27:50 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>DrugShoppa</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+
+  	<div class="wrap">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 d-flex align-items-center">
+						<p class="mb-0 phone pl-md-2">
+							<a href="#" class="mr-2"><span class="fa fa-phone mr-1"></span> +00 1234 567</a> 
+							<a href="#"><span class="fa fa-paper-plane mr-1"></span> youremail@email.com</a>
+						</p>
+					</div>
+					<div class="col-md-6 d-flex justify-content-md-end">
+						<div class="social-media">
+			    		<p class="mb-0 d-flex">
+			    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+			    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
+			    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
+			    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a>
+			    		</p>
+		        </div>
+					</div>
+				</div>
+			</div>
+		</div>
+    
+	  <nav class="navbar navbar-expand-lg navbar-light ftco_navbar bg-light ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	      <a class="navbar-brand" href="index.php">DrugShoppa</a>
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="oi oi-menu"></span> Menu
+	      </button>
+
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav ml-auto">
+	          <li class="nav-item active"><a href="index.php" class="nav-link">Home</a></li>
+	          <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
+	          <li class="nav-item"><a href="practice-area.php" class="nav-link">Practice Areas</a></li>
+	          <li class="nav-item"><a href="cases.php" class="nav-link">Cases</a></li>
+	          <li class="nav-item"><a href="attorney.php" class="nav-link">Attorneys</a></li>
+	          <li class="nav-item"><a href="blog.php" class="nav-link">Blog</a></li>
+	          <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+    
+    <div class="hero-wrap" style="background-image: url('images/pharmacy.jpg');" data-stellar-background-ratio="0.5">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center">
+          <div class="col-md-6 ftco-animate d-flex align-items-end">
+          	<div class="text w-100">
+	            <h1 class="mb-4">Don't Feel Helpless We Fight for Justice</h1>
+	            <p class="mb-4">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+	            <p><a href="#" class="btn btn-primary py-3 px-4">Contact us</a> <a href="#" class="btn btn-white py-3 px-4">Read more</a></p>
+            </div>
+          </div>
+          <a href="https://vimeo.com/45830194" class="img-video popup-vimeo d-flex align-items-center justify-content-center">
+          	<span class="fa fa-play"></span>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <section class="ftco-intro">
+    	<div class="fluid">
+    		<div class="row no-gutters">
+    			<div class="col-md-3 d-flex">
+    				<div class="intro aside-stretch d-lg-flex w-100">
+    					<div class="icon">
+    						<span class="fa fa-certificate center"></span>
+    					</div>
+    					<div class="text py-4">
+    						<h2>Operated by Licensed Pharmacy</h2>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-md-3 d-flex">
+    				<div class="intro color-1 d-lg-flex w-100">
+    					<div class="icon">
+    						<span class="fa fa-check"></span>
+    					</div>
+    					<div class="text py-4">
+    						<h2>Original Drugs</h2>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-md-2 d-flex">
+    				<div class="intro color-2 d-lg-flex w-100">
+    					<div class="icon">
+    						<span class="fa fa-money"></span>
+    					</div>
+    					<div class="text py-4">
+    						<h2>Fair Price</h2>
+    					</div>
+    				</div>
+    			</div>
+    			<div class="col-md-2 d-flex">
+    				<div class="intro color-3 d-lg-flex w-100">
+    					<div class="icon">
+    						<span class="fa fa-truck"></span>
+    					</div>
+    					<div class="text py-4">
+    						<h2>Pick Up Options</h2>
+    					</div>
+    				</div>
+    			</div>
+          <div class="col-md-2 d-flex">
+            <div class="intro color-3 d-lg-flex w-100">
+              <div class="icon">
+                <span class="fa fa-credit-card"></span>
+              </div>
+              <div class="text py-4">
+                <h2>Easy Payment</h2>
+              </div>
+            </div>
+          </div>
+    		</div>
+    	</div>
+    </section>
+
+    <section class="ftco-section ftco-no-pb ftco-no-pt" >
+			<div class="container">
+				<div class="row no-gutters">
+					<div class="col-md-6 d-flex align-items-stretch">
+						<div class="img img-3 w-100 d-flex justify-content-center align-items-center" style="background-image: url(images/cu.png); position: relative;">
+						</div>
+					</div>
+					<div class="col-md-6 wrap-about ftco-animate">
+						<div class="bg-light px-3 px-md-5 py-5 ">
+		          <div class="heading-section">
+		            <h2 class="mb-3">Chat with a pharmacist</h2>
+
+		            <p class="med">Order Medication or Medical Supply at any Time</p>
+                <a href="" class="btn btn-primary">Shop by Category</a>
+		          </div>
+
+		          
+	          </div>
+
+					</div>
+				</div>
+			</div>
+		</section>
+    <section class="ftco-section ftco-no-pb ftco-no-pt" >
+      <div class="container">
+        <div class="row no-gutters">
+          <div class="col-md-6 wrap-about ftco-animate">
+            <div class="bg-light px-3 px-md-5 py-5 ">
+              <div class="heading-section">
+                <h2 class="mb-3">A Special Kind of Care</h2>
+
+                <p class="med">Our Membership services cater for paitiens who need refill on periodic bases.we will work with you to ensure you never miss a dose.</p>
+                <a href="" class="btn btn-primary">Sign Up Today</a>
+              </div>
+
+              
+            </div>
+
+          </div>
+          <div class="col-md-6 d-flex align-items-stretch">
+            <div class="img img-3 w-100 d-flex justify-content-center align-items-center" style="background-image: url(images/family.jpg); position: relative;">
+          </div>
+        </div>
+      </div>
+    </section><br>
+    <section class="ftco-section ftco-no-pb ftco-no-pt" >
+      <div class="container">
+        <div class="row no-gutters justify-content-center align-items-center">
+          <div class=" wrap-about ftco-animate justify-content-center align-items-center">
+            <div class="bg-light px-3 px-md-5 py-5 ">
+              <div class="heading-section">
+                <img src="images/phone.png" style="border-radius: 1.5em; align-items: center; justify-content: center;">
+
+                <div class="py-5">
+                  <img src="images/apple.png" style="width: 140px; height: 60px; border-radius: 0.6em;">
+                  <img src="images/android.png" style="width: 140px; height: 60px; border-radius: 0.6em;">
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </section><br>
+    
+		
+
+    <footer class="ftco-footer">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2 logo"><a href="#">DrugShoppa</a></h2>
+              <p>Far far away, behind the word mountains, far from the countries.</p>
+              <ul class="ftco-footer-social list-unstyled mt-2">
+                <li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2">Explore</h2>
+              <ul class="list-unstyled">
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>About</a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Contact</a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Practice Areas</a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Cases</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4 ml-md-4">
+              <h2 class="ftco-heading-2">Legal</h2>
+              <ul class="list-unstyled">
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Join us</a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Blog</a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Privacy &amp; Policy</a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Term &amp; Conditions</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+             <div class="ftco-footer-widget mb-4">
+              <h2 class="ftco-heading-2">Company</h2>
+              <ul class="list-unstyled">
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>About Us</a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Blog</a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Contact</a></li>
+                <li><a href="#"><span class="fa fa-chevron-right mr-2"></span>Careers</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+            <div class="ftco-footer-widget mb-4">
+            	<h2 class="ftco-heading-2">Have a Questions?</h2>
+            	<div class="block-23 mb-3">
+	              <ul>
+	                <li><span class="fa fa-map-marker pr-2"></span><span class="text">203 Address</span></li>
+	                <li><a href="#"><span class="fa fa-phone pr-2"></span><span class="text">+2 392 3929 210</span></a></li>
+	                <li><a href="#"><span class="fa fa-paper-plane pr-2"></span><span class="text">info@yourdomain.com</span></a></li>
+	              </ul>
+	            </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid px-0 py-5 bg-black">
+      	<div class="container">
+      		<div class="row">
+	          <div class="col-md-12">
+		
+	            <p class="mb-0" style="color:#4798D4;">
+	  Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved by <a href="https://ouchestechnology.xyz" target="_blank">Ouches Technology</a></p>
+	          </div>
+	        </div>
+      	</div>
+      </div>
+    </footer>
+    
+  
+
+  <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
